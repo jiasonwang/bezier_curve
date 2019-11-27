b7020d5ea9a42a84b98556c7d133a71b6ed472fb
jason wang <wangstevens0412@gmail.com>
2019-11-27 23:40:24 +0800

    add beziercurv 3

---
+++ src/beziercurv.ts
@@ -0,0 +1,4 @@
+import { Position } from "./model"
+function calculacteCtrlPoints(p0: Position, p1: Position, p2: Position, p3: Position): Array<Position> {
+    return null;
+}
--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,9 @@
-export class Position{
-    constructor(public x:number,public y:number){}
-    toString(){
+export class Position {
+    constructor(public x: number, public y: number) { }
+    toString() {
         return `{x:${this.x},y:${this.y}}`
+    }
+    middle(other: Position): Position {
+        return new Position((other.x + this.x) / 2, (other.y + this.y) / 2);
     }
 }
